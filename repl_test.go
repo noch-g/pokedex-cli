d5a1e249aabb72ba827343939b26a2caaeb47563
noch-g <ladle.doings_0g@icloud.com>
2025-02-05 17:59:47 +0100

    test: add test of valid commands

--- repl_test.go
+++ repl_test.go
@@ -2,20 +2,73 @@ package main
 
 import (
 	"bytes"
-	"fmt"
+	"strings"
 	"testing"
+	"time"
+
+	"github.com/noch-g/pokedex-cli/internal/pokeapi"
 )
 
-func TestStartRepl_ValidCommand(t *testing.T) {
-	cfg := &config{}
-	input := "help\nexit\n" // Simule l'entrée de l'utilisateur
-	in := bytes.NewBufferString(input)
-	out := new(bytes.Buffer)
+type commandTestCase struct {
+	name     string
+	input    string
+	expected []string
+}
 
-	startRepl(cfg, in, out)
+func TestStartRepl_ValidCommands(t *testing.T) {
+	pokeClient := pokeapi.NewClient(5*time.Second, 5*time.Minute)
 
-	result := out.String()
-	fmt.Println(result)
+	cfg := &config{
+		CaughtPokemon: map[string]pokeapi.Pokemon{},
+		knownEntities: map[string][]string{
+			"commands":     {},
+			"pokemons":     {},
+			"locations":    {},
+			"wildPokemons": {},
+		},
+		pokeapiClient: pokeClient,
+	}
+	commands := []commandTestCase{
+		{
+			name:     "Check mapb before any location loaded",
+			input:    "mapb",
+			expected: []string{"you're on the first page"},
+		},
+		{
+			name:     "Load locations with map",
+			input:    "map",
+			expected: []string{"canalave-city-area"},
+		},
+		{
+			name:     "Load locations with map",
+			input:    "map",
+			expected: []string{"great-marsh-area-1"},
+		},
+		{
+			name:     "Check mapb after locations loaded",
+			input:    "mapb",
+			expected: []string{"canalave-city-area", "eterna-city-area"},
+		},
+	}
+
+	for _, cmd := range commands {
+		t.Run(cmd.name, func(t *testing.T) {
+			in := bytes.NewBufferString(cmd.input + "\nexit\n")
+			out := new(bytes.Buffer)
+
+			startRepl(cfg, in, out)
+
+			result := out.String()
+			for _, expected := range cmd.expected {
+				if !strings.Contains(result, expected) {
+					t.Errorf("\nCommand: %q\nExpected: %s\nGot: %s",
+						cmd.input,
+						expected,
+						result)
+				}
+			}
+		})
+	}
 }
 
 func TestCleanInput(t *testing.T) {
