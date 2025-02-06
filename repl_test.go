2de842fd90917346c44e8816574e310f2ab6979b
noch-g <ladle.doings_0g@icloud.com>
2025-02-06 18:44:50 +0100

    test: improve valid command tests with the use of pipes

--- repl_test.go
+++ repl_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
-	"bytes"
+	"bufio"
+	"fmt"
+	"io"
 	"strings"
 	"testing"
 	"time"
@@ -16,58 +18,100 @@ type commandTestCase struct {
 }
 
 func TestStartRepl_ValidCommands(t *testing.T) {
-	pokeClient := pokeapi.NewClient(5*time.Second, 5*time.Minute)
-
-	cfg := &config{
-		CaughtPokemon: map[string]pokeapi.Pokemon{},
-		knownEntities: map[string][]string{
-			"commands":     {},
-			"pokemons":     {},
-			"locations":    {},
-			"wildPokemons": {},
-		},
-		pokeapiClient: pokeClient,
-	}
+	cfg := setupTestConfig()
 	commands := []commandTestCase{
 		{
 			name:     "Check mapb before any location loaded",
 			input:    "mapb",
-			expected: []string{"you're on the first page"},
+			expected: []string{GetPromptMessage() + "mapb", "you're on the first page"},
 		},
 		{
 			name:     "Load locations with map",
 			input:    "map",
-			expected: []string{"canalave-city-area"},
+			expected: []string{GetPromptMessage() + "map", "canalave-city-area"},
 		},
 		{
-			name:     "Load locations with map",
+			name:     "Load locations with map x2",
 			input:    "map",
-			expected: []string{"great-marsh-area-1"},
+			expected: []string{GetPromptMessage() + "map", "mt-coronet-1f-route-216"},
 		},
 		{
 			name:     "Check mapb after locations loaded",
 			input:    "mapb",
-			expected: []string{"canalave-city-area", "eterna-city-area"},
+			expected: []string{GetPromptMessage() + "mapb", "canalave-city-area", "eterna-city-area"},
 		},
 	}
 
+	inR, inW := io.Pipe()
+	outR, outW := io.Pipe()
+
+	go startRepl(cfg, inR, outW)
+
+	// Make this higher if cmd is expected to output more lines
+	outputChan := make(chan string, 100)
+	go func() {
+		reader := bufio.NewReader(outR)
+		for {
+			line, err := reader.ReadString('\n')
+			if err != nil {
+				close(outputChan)
+				return
+			}
+			outputChan <- line
+		}
+	}()
+
 	for _, cmd := range commands {
 		t.Run(cmd.name, func(t *testing.T) {
-			in := bytes.NewBufferString(cmd.input + "\nexit\n")
-			out := new(bytes.Buffer)
-
-			startRepl(cfg, in, out)
-
-			result := out.String()
-			for _, expected := range cmd.expected {
-				if !strings.Contains(result, expected) {
-					t.Errorf("\nCommand: %q\nExpected: %s\nGot: %s",
-						cmd.input,
-						expected,
-						result)
+			fmt.Fprintf(inW, "%s\n", cmd.input)
+			var output []string
+			timeout := time.After(5 * time.Second)
+		collectNbExpectedLines:
+			for len(output) < len(cmd.expected) {
+				select {
+				case line, ok := <-outputChan:
+					if !ok {
+						break
+					}
+					output = append(output, line)
+				case <-timeout:
+					t.Errorf("Timeout waiting for command %s output. Got %d lines, expected %d",
+						cmd.input, len(output), len(cmd.expected))
+					break collectNbExpectedLines
 				}
 			}
+
+			if !areAllExpectedFound(output, cmd) {
+				var errorStr strings.Builder
+				errorStr.WriteString(fmt.Sprintf("Cmd %s failed,\n", cmd.input))
+				errorStr.WriteString(fmt.Sprintf("Expected:\n%s\n", strings.Join(cmd.expected, "\n")))
+				errorStr.WriteString(fmt.Sprintf("Received:\n%s\n", strings.Join(output, "")))
+				t.Error(errorStr.String())
+			}
+
+			flushOutputChan(outputChan)
 		})
+	}
+
+	// Clean up
+	fmt.Fprintf(inW, "exit\n")
+	inW.Close()
+	outW.Close()
+}
+
+func flushOutputChan(outputChan <-chan string) {
+	for {
+		select {
+		case <-outputChan:
+		default:
+			// If there is no data in the channel for 500ms, consider it flushed
+			time.Sleep(500 * time.Millisecond)
+			select {
+			case <-outputChan:
+			default:
+				return
+			}
+		}
 	}
 }
 
@@ -100,4 +144,35 @@ func TestCleanInput(t *testing.T) {
 			}
 		}
 	}
+}
+
+func setupTestConfig() *config {
+	pokeClient := pokeapi.NewClient(5*time.Second, 5*time.Minute)
+	return &config{
+		CaughtPokemon: map[string]pokeapi.Pokemon{},
+		knownEntities: map[string][]string{
+			"commands":     {},
+			"pokemons":     {},
+			"locations":    {},
+			"wildPokemons": {},
+		},
+		pokeapiClient: pokeClient,
+	}
+}
+
+func areAllExpectedFound(output []string, cmd commandTestCase) bool {
+	for _, expected := range cmd.expected {
+		found := false
+		for _, out := range output {
+			if strings.Contains(out, expected) {
+				found = true
+				break
+			}
+		}
+		if !found {
+			fmt.Println("Could not find expected output:", expected)
+			return false
+		}
+	}
+	return true
 }
