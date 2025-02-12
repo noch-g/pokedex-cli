7726c91fbaff4685d0c55e24414c702e94996c24
noch-g <ladle.doings_0g@icloud.com>
2025-02-12 22:42:01 +0100

    feat: allow option for different kind of balls with higher success rates

--- internal/commands/command_catch.go
+++ internal/commands/command_catch.go
@@ -7,13 +7,26 @@ import (
 	"math/rand"
 
 	"github.com/noch-g/pokedex-cli/internal/config"
+	"github.com/noch-g/pokedex-cli/internal/settings"
 )
 
 func commandCatch(cfg *config.Config, output io.Writer, args ...string) error {
-	if len(args) != 1 {
+	if len(args) < 1 {
 		return errors.New("you must provide a pokemon name")
 	}
 	pokemonName := args[0]
+	ballType := "Pokeball"
+
+	for _, arg := range args[1:] {
+		if arg == "--super-ball" {
+			ballType = "Superball"
+		} else if arg == "--hyper-ball" {
+			ballType = "Hyperball"
+		} else if arg == "--master-ball" {
+			ballType = "Masterball"
+		}
+	}
+	catchThreshold := settings.CatchThreshold[ballType]
 
 	pokemon, err := cfg.PokeapiClient.GetPokemon(pokemonName)
 	if err != nil {
@@ -21,10 +34,10 @@ func commandCatch(cfg *config.Config, output io.Writer, args ...string) error {
 	}
 
 	res := rand.Intn(pokemon.BaseExperience)
-	successChance := 100 * 40.0 / float64(pokemon.BaseExperience)
+	successChance := min(100, 100*float64(catchThreshold)/float64(pokemon.BaseExperience))
 
-	fmt.Fprintf(output, "Throwing a Pokeball at %s... (success chance: %.2f%%)\n", pokemon.Name, successChance)
-	if res > 40 {
+	fmt.Fprintf(output, "Throwing a %s at %s... (success chance: %.2f%%)\n", ballType, pokemon.Name, successChance)
+	if res > catchThreshold {
 		escapeMessages := []string{
 			"Fail, %s broke free!",
 			"Fail, %s slipped away!",
--- internal/settings/settings.go
+++ internal/settings/settings.go
@@ -1,7 +1,16 @@
 package settings
 
+import "math"
+
 const (
 	Prompt        = "Pokedex > "
 	SaveFilePath  = "pokemons.json"
 	CacheFilePath = "cache.gob"
 )
+
+var CatchThreshold = map[string]int{
+	"Pokeball":   40,
+	"Superball":  60,
+	"Hyperball":  85,
+	"Masterball": math.MaxInt,
+}
